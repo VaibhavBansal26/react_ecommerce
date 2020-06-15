0537955e8c2b5c7637e6c294434a9753a66cc105
Vaibhav Bansal <vaibhav.bansal945@gmail.com>
2020-06-15 14:03:45 +0530

    done

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
+
 import {selectCartItems} from '../../redux/cart/cart.selectors';
 import {selectCartTotal} from '../../redux/cart/cart.selectors';
 import './checkout.styles.scss';
@@ -39,6 +41,7 @@ const CheckoutPage = ({cartItems,total}) => (
         <div className= 'total'>
         <span>Total : $ {total}</span>
         </div>
+        <StripeCheckoutButton price={total}/>
     
     </div>
 )
